98a171d24ce34e7bc5730af9d36759c087573157
plumfive <alex.planelles.a@gmail.com>
2026-04-30 12:38:58 +0200

    cutom welcome message

--- init.lua
+++ init.lua
@@ -5,7 +5,11 @@ require("config.autocommands")
 require("config.neovide")
 require("config.highlights")
 require("config.lazy") -- Plugin Manager
-require("noice").notify("Hello from Alex P config", "info")
+require("noice").notify("Hello from Alex P config", "info", {
+    title = "Welcome",
+    timeout = 1000,
+    render = "minimal",
+})
 
 -- Aixo d'aqui ho fa folke/todo-comments.nvim
 -- TODO: procrastinar
