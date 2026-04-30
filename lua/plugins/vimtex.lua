5004aae5826cf78385bca4d05e17ffbc4e6edbdc
plumfive <alex.planelles.a@gmail.com>
2026-04-30 12:40:13 +0200

    comment changes

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,14 +1,14 @@
 return {
     'lervag/vimtex',
+    dependencies = { "L3MON4D3/LuaSnip" },
     config = function()
-        vim.g.vimtex_view_method = 'general'        -- use SumatraPDF
-        vim.g.vimtex_compiler_method = 'latexmk'    -- use your .latexmkrc
-	vim.g.vimtex_quickfix_ignore_filters = {
-	    'translations-basic-dictionary-catalan',
+        vim.g.vimtex_view_method = 'general' -- Use SumatraPDF for pdf view
+        vim.g.vimtex_compiler_method = 'latexmk' -- Use the .latexmkrc file
+        vim.g.vimtex_quickfix_ignore_filters = {
+            'translations-basic-dictionary-catalan',
 	} -- ignore warning: catalan not appearing in siunix package.
     end
 }
-
 
 
 
